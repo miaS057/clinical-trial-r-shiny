4e7685fb8bae6d332fe942e84b7a01584c753bb0
Ethan Baek <baeke2@uci.edu>
2025-09-02 15:39:31 -1000

    Made a server branch for when we are working on the server.R (back end) for clean git set up

--- server.R
+++ server.R
@@ -1,3 +1,10 @@
 # Load libraries
 
+# For now keep just use server.R with pharmaversesdtm
+# Later on we can switch it so they can import a file from user input on the UI
+
+if (!requireNamespace("pharmaverse")) {
+  install.packages("pharmaverse")
+}
+library(pharmaverse)
 
