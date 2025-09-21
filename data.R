03b206f63353b177ab38d57379566fcf675cb8cf
Mia Sohn <miasohn@icloud.com>
2025-09-20 17:49:05 -0700

    Added data folder and data.R

---
+++ data.R
@@ -0,0 +1,19 @@
+get_dm <- function(){
+  dm <- read.csv("data/dm.csv")
+  return(dm)
+}
+
+get_ds <- function(){
+  ds <- read.csv("data/ds.csv")
+  return(ds)
+}
+
+get_ae <- function(){
+  ae <- read.csv("data/ae.csv")
+  return(ae)
+}
+
+get_cm <- function(){
+  cm <- read.csv("data/cm.csv")
+  return(cm)
+}
