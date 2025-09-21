81f682866015087a96035de1c11eb53229d98f5f
Mia Sohn <miasohn@icloud.com>
2025-09-20 17:50:48 -0700

    Changed server to import data.R and use functions to get each dataset from the data folder

--- server.R
+++ server.R
@@ -4,19 +4,27 @@ library(shiny)
 # For now keep just use server.R with pharmaversesdtm
 # Later on we can switch it so they can import a file from user input on the UI
 
-if (!requireNamespace("pharmaversesdtm")) {
-  install.packages("pharmaversesdtm")
-}
-library(pharmaversesdtm)
+# if (!requireNamespace("pharmaversesdtm")) {
+#   install.packages("pharmaversesdtm")
+# }
+# library(pharmaversesdtm)
+
+# --- Import data.R --- #
+source("data.R")
+
 
 
 server <- function(input, output, session) {
   
   # load in each dataset
-  data("dm", package = "pharmaversesdtm")
-  data("ds", package = "pharmaversesdtm")
-  data("ae", package = "pharmaversesdtm")
-  data("cm", package = "pharmaversesdtm")
+  dm <- get_dm()
+  ds <- get_ds()
+  ae <- get_ae()
+  cm <- get_cm()
+  # data("dm", package = "pharmaversesdtm")
+  # data("ds", package = "pharmaversesdtm")
+  # data("ae", package = "pharmaversesdtm")
+  # data("cm", package = "pharmaversesdtm")
   
   # make them reactive so we can import them into each module
   dm_r <- reactive(dm)
@@ -30,13 +38,16 @@ server <- function(input, output, session) {
   # we need to export the data we need from the server to each module
   # NOTE: So for each module we need to have a server component
   
-  mod_dm_server("dm", dm_r = dm_r)
+  mod_dm_server(input, output, dm_r = dm_r)
+  #mod_ds_server(output, ds_r = ds_r)
+  mod_ae_server(input, output, dm_r = dm_r, ae_r = ae_r)
+  #mod_cm_server(output, cm_r = cm_r)
   
   # NOTE: if your particular component needs other data_sets
   # you can add them here
-  mod_ds_server(dm_r = dm_r, ds_r = ds_r)
-  mod_ae_server(dm_r = dm_r, ae_r = ae_r)
-  mod_cm_server(dm_r = dm_r, cm_r = cm_r)
+  # mod_ds_server(dm_r = dm_r, ds_r = ds_r)
+  # mod_ae_server(dm_r = dm_r, ae_r = ae_r)
+  # mod_cm_server(dm_r = dm_r, cm_r = cm_r)
   
   # READ THIS: So in each module you can get the data from the server by doing
   # mod_dm_server <- function(dm_r) {
